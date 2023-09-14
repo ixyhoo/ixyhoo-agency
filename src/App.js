3ac080d30eb89f1393cf2567364591ed6b06cce0
ixyhoo <ixyhoo@outlook.fr>
2023-09-14 14:13:10 +0200

    I make not found page

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import Project2 from './pages/Project2';
 import Project3 from './pages/Project3';
 import Project4 from './pages/Project4';
 import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
    <BrowserRouter>
    <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/projet-1" element={<project1 />} />
+      <Route path="/projet-1" element={<Project1 />} />
       <Route path="/projet-2" element={<Project2 />} />
       <Route path="/projet-3" element={<Project3/>} />
       <Route path="/projet-4" element={<Project4 />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<NotFound />} />
    </Routes>
    </BrowserRouter>
   );
