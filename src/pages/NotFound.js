40fa4e8e9be25af358aedf98a190038aa9893cb7
ixyhoo <ixyhoo@outlook.fr>
2023-09-14 14:24:12 +0200

    I done not found page style

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';         
 
 const NotFound = () => {
     return (
        <div className="notFound">
         <div className="notFound-content">
             <h2>erreur 404</h2>
+           <NavLink to="/">
+            <h3>Retour à L'acueil <i className='fas fa-home'></i> </h3>
+           </NavLink>
         </div>
        </div>
     );
