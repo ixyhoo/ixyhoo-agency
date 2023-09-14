99a3e1019536367275712be6343b8ac741a0f656
ixyhoo <ixyhoo@outlook.fr>
2023-09-14 10:02:47 +0200

    I create contact page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-       <div className="notFound"></div>
+       <div className="notFound">
+        <div className="notFound-content">
+            <h2>erreur 404</h2>
+        </div>
+       </div>
     );
 };
 
